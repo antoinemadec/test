cc365fc91984f4f970fdb5f382595228af30d421
antoinemadec <ajamadec@gmail.com>
2019-02-21 15:57:56 -0800

    Python: codewar: progress but infinite regression

--- python/codewars/stargate_sg-1_cute_and_fuzzy_improved_version/stargate_sg-1_cute_and_fuzzy_improved_version.py
+++ python/codewars/stargate_sg-1_cute_and_fuzzy_improved_version/stargate_sg-1_cute_and_fuzzy_improved_version.py
@@ -24,27 +24,46 @@ NEIGHBORHOOD = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1
 
 
 class Node:
-    def __init__(self, pos, goal=0):
+    def __init__(self, pos, is_goal=0):
         self.pos = pos
-        self.goal = goal
+        self.is_goal = is_goal
         self.children = []
+        self.shortest_path_to_goal = None
 
     def add_child(self, obj):
         self.children.append(obj)
 
-    def find_branch(self):
-        print("COUCOU")
+    def find_shortest_path_to_goal(self, parent_pos=[]):
+        """returns shortest [distance, [(self.pos),... ,(pos_goal)]]"""
+        if self.shortest_path_to_goal is None:
+            if self.is_goal:
+                self.shortest_path_to_goal = [0, [self.pos]]
+            else:
+                paths = []
+                for c in [x for x in self.children if x != parent_pos]:
+                    p = c.find_shortest_path_to_goal(self.pos)
+                    if p != []:
+                        [child_distance, child_path] = p
+                        if self.pos not in child_path:
+                            delta_d = ((c.pos[0]-self.pos[0])**2 + (c.pos[1]-self.pos[1])**2)**.5
+                            paths.append([child_distance + delta_d, [self.pos] + child_path])
+                if paths == []:
+                    self.shortest_path_to_goal = []
+                else:
+                    paths.sort()
+                    self.shortest_path_to_goal = paths[0]
+        return self.shortest_path_to_goal
 
 
 class Tree:
     def __init__(self, m):
         self.m = m
         self.node_dict = {}
-        self.start = None
+        self.start_pos = None
 
-    def create_node(self, pos, goal=0):
+    def create_node(self, pos, is_goal=0):
         if pos not in self.node_dict:
-            self.node_dict[pos] = Node(pos, goal)
+            self.node_dict[pos] = Node(pos, is_goal)
 
     def populate_tree(self):
         X = len(self.m)
@@ -54,13 +73,17 @@ class Tree:
                 if self.m[x][y] == '.' or self.m[x][y] == 'S':
                     self.create_node((x, y))
                     if self.m[x][y] == 'S':
-                        self.start = (x, y)
+                        self.start_pos = (x, y)
                     for vec in NEIGHBORHOOD:
                         (k, l) = (x+vec[0], y+vec[1])
                         if k in range(X) and l in range(Y):
                             self.create_node((k, l))
+                            self.node_dict[(x, y)].add_child(self.node_dict[(k, l)])
                 elif self.m[x][y] == 'G':
-                    self.create_node((x, y), goal=1)
+                    self.create_node((x, y), is_goal=1)
+
+    def find_shortest_path_to_goal(self):
+        return self.node_dict[self.start_pos].find_shortest_path_to_goal()
 
 
 def wire_DHD_SG1(existingWires):
@@ -68,8 +91,7 @@ def wire_DHD_SG1(existingWires):
     m = [[l for l in s] for s in existingWires.split()]
     t = Tree(m)
     t.populate_tree()
-    print(len(t.node_dict))
-    # t.find_branches()
+    path = t.find_shortest_path_to_goal()
     # if Node.possible_paths == []:
     #     return "Oh for crying out loud..."
     # else:
@@ -140,4 +162,4 @@ def gen_input_pattern(n=73):
     return s
 
 
-print(wire_DHD_SG1(gen_input_pattern()))
+print(wire_DHD_SG1(gen_input_pattern(3)))
