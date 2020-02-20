2093c655c225d46fb66c291cb60938e36e6f33ba
antoinemadec <ajamadec@gmail.com>
2020-02-19 18:28:35 -0800

    JS: add operators

---
+++ js/learn_js/operators/operators.js
@@ -0,0 +1,23 @@
+// The addition operator
+var a = 1;
+var b = 2;
+var c = a + b;
+console.log(c);
+var name = "John";
+console.log("Hello " + name + "!");
+console.log("The meaning of life is " + 42);
+console.log(42 + " is the meaning of life");
+console.log(1 + "1");   // outputs "11"
+console.log("1" + 1);   // outputs "11"
+
+// other ops
+console.log(3 - 5);     // outputs -2
+console.log(3 * 5);     // outputs 15
+console.log(3 / 5);     // outputs 0.6
+console.log(5 % 3);     // outputs 2
+
+// Math
+console.log(Math.abs(-5));
+console.log(Math.exp(1));
+console.log(Math.pow(2,10));
+console.log(Math.floor(2.2));
