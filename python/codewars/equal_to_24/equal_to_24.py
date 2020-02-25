cae113274b9a762e9df0b3591a59af44e6891e41
antoinemadec <ajamadec@gmail.com>
2020-02-25 12:33:03 -0800

    Python: update equal_to_24

--- python/codewars/equal_to_24/equal_to_24.py
+++ python/codewars/equal_to_24/equal_to_24.py
@@ -6,26 +6,26 @@ l1 = ('+', '-', '*', '/')
 l2 = [[op0, op1] for op0 in l1 for op1 in l1]
 l3 = [[op0] + op1 for op0 in l1 for op1 in l2]
 
-def insert_parenthesis(base_s, idx0, idx1):
-    s = base_s[:idx1] + ')' + base_s[idx1:]
-    s = s[:idx0] + '(' + s[idx0:]
-    return s
+def insert_parenthesis(base_l, idx0, idx1):
+    cpy_l = base_l[:]
+    cpy_l.insert(idx1, ')')
+    cpy_l.insert(idx0, '(')
+    return cpy_l
 
 def equal_to_24(a,b,c,d):
     s = set(permutations((str(a),str(b),str(c),str(d))))
     for p in s:
         for ops in l3:
-            string = p[0] + ops[0] + p[1] + ops[1] + p[2] + ops[2] + p[3]
-            strings = [string]
-            s03 = insert_parenthesis(string, 0, 3)
-            s0369 = insert_parenthesis(s03, 6, 9)
-            strings += [s03]
-            strings += [s0369]
-            strings += [insert_parenthesis(string, 0, 5)]
-            strings += [insert_parenthesis(string, 2, 5)]
-            if ops == ['+', '*', '-'] and p == ('1', '1', '1', '13'):
-                print(strings)
-            for s in strings:
+            l_str = [p[0], ops[0], p[1], ops[1], p[2], ops[2], p[3]]
+            l_strs = [l_str]
+            l_s03 = insert_parenthesis(l_str, 0, 3)
+            l_s0369 = insert_parenthesis(l_s03, 6, 9)
+            l_strs += [l_s03]
+            l_strs += [l_s0369]
+            l_strs += [insert_parenthesis(l_str, 0, 5)]
+            l_strs += [insert_parenthesis(l_str, 2, 5)]
+            for l_s in l_strs:
+                s = "".join(l_s)
                 try:
                     if eval(s) == 24:
                         return s
@@ -39,4 +39,5 @@ def equal_to_24(a,b,c,d):
 # print(equal_to_24(1,1,1,1))     # should return "It's not possible!"
 # print(equal_to_24(13,13,13,13)) # should return "It's not possible!"
 
-print(equal_to_24(1,1,1,13))     # (1+1)*(13-1)
+print(equal_to_24(1,1,1,13))    # (1+1)*(13-1)
+print(equal_to_24(1,3,4,6))     # 6/(1-(3/4))
