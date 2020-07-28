d11b866ba5db9e6f77348fef5021578df46b77c0
antoinemadec <ajamadec@gmail.com>
2020-07-28 10:47:56 -0700

    JS: ray casting better perf

--- js/coding_train/ray_casting/P5/ray.js
+++ js/coding_train/ray_casting/P5/ray.js
@@ -1,14 +1,32 @@
 class Ray {
   constructor(
-    pos,
-    angle,
+    pos = createVector(0, 0),
+    angle = 0,
     hsb = [0, 100, 100],
     max_reflections = 10,
-    reflection_idx = 0
+    reflection_idx = -1
   ) {
     this.hsb = hsb;
     this.reflection_idx = reflection_idx;
     this.max_reflections = max_reflections;
+    this.reflections = [];
+    // create reflections array if "top" ray
+    if (reflection_idx == -1) {
+      let brightness = hsb[2];
+      for (var i = 0; i < max_reflections; i++) {
+        brightness *= 0.9;
+        this.reflections.push(
+          new Ray(
+            createVector(0, 0),
+            0,
+            [hsb[0], hsb[1], brightness],
+            max_reflections,
+            i
+          )
+        );
+        this.reflections[i].reflections = this.reflections;
+      }
+    }
     this.update(pos, angle);
   }
 
@@ -17,7 +35,6 @@ class Ray {
     this.angle = angle;
     this.dir = p5.Vector.fromAngle(radians(this.angle));
     this.inf_dir = this.dir.copy().mult(2 * max(width, height));
-    this.reflection = null;
   }
 
   show(walls) {
@@ -29,18 +46,19 @@ class Ray {
       if (r) {
         const pt = r[0];
         const refl_vect = r[1];
-        const d = p5.Vector.dist(this.pos, pt);
-        if (d < record && d >= 0.9) {
-          record = d;
+        const dist_square =
+          pow(this.pos.x - pt.x, 2) + pow(this.pos.y - pt.y, 2);
+        if (dist_square < record && dist_square >= 0.9) {
+          record = dist_square;
           closest_point = pt;
           closest_refl_vect = refl_vect;
         }
       }
     }
     if (closest_point) {
-      this.reflection = this.createReflection(closest_point, closest_refl_vect);
-      if (this.reflection) {
-        this.reflection.show(walls);
+      let reflection = this.createReflection(closest_point, closest_refl_vect);
+      if (reflection) {
+        reflection.show(walls);
       }
       colorMode(HSB, 100);
       stroke(this.hsb[0], this.hsb[1], this.hsb[2]);
@@ -76,11 +94,13 @@ class Ray {
     const u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den;
     if (t > 0 && t < 1 && u > 0) {
       // reflection vector
-      const v1 = createVector(x4 - x3, y4 - y3).normalize();
-      const v2 = createVector(x2 - x1, y2 - y1).normalize();
-      const k = p5.Vector.dot(v1, v2) / p5.Vector.dot(v2, v2);
-      const k2_v2 = v2.copy().mult(2 * k);
-      const refl_vect = v1.copy().mult(-1).add(k2_v2);
+      const v1 = [x4 - x3, y4 - y3];
+      const v2 = [x2 - x1, y2 - y1];
+      const k = (v1[0] * v2[0] + v1[1] * v2[1]) / (v2[0] * v2[0] + v2[1] * v2[1]);
+      const refl_vect = createVector(
+        -v1[0] + 2 * k * v2[0],
+        -v1[1] + 2 * k * v2[1]
+      );
       // intersection point
       const pt = createVector();
       pt.x = x1 + t * (x2 - x1);
@@ -95,17 +115,12 @@ class Ray {
     const pos = point;
     const angle = degrees(createVector(1, 0).angleBetween(vect));
     const reflection_idx = this.reflection_idx + 1;
-    const brightness = this.hsb[2] * 0.9;
-    if (brightness <= 0 || reflection_idx > this.max_reflections) {
+    if (reflection_idx + 1 > this.max_reflections) {
       return null;
     } else {
-      return new Ray(
-        pos,
-        angle,
-        [this.hsb[0], this.hsb[1], brightness],
-        this.max_reflections,
-        reflection_idx
-      );
+      let reflection = this.reflections[reflection_idx];
+      reflection.update(pos, angle);
+      return reflection;
     }
   }
 }
