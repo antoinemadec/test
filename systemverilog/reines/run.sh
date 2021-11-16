fa4b304e44f6262f6004817e8057080ba8adad99
EXTERNAL MADEC Antoine (ELSYS, AE/EIY2) <external.Antoine.MADEC@fr.bosch.com>
2021-11-16 13:10:40 +0100

    SV: add chess exercise

---
+++ systemverilog/reines/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+time xrun -64 -sv test.sv
---
+++ systemverilog/reines/test.sv
@@ -0,0 +1,56 @@
+class queens;
+  localparam N = 100;
+
+  rand bit arr[N][N];
+
+  constraint c {
+    foreach (arr[i, j]) {
+      if (arr[i][j] == 1) {
+        // only one 1 in a col
+        foreach (arr[ , l]) {
+          if (j != l) {
+            arr[i][l] == 0;
+          }
+        }
+
+        // only one 1 in a row
+        foreach (arr[k, ]) {
+          if (i != k) {
+            arr[k][j] == 0;
+          }
+        }
+
+        // only on 1 in a diagonal
+        foreach (arr[k,l]) {
+          if ((((i-k) == (j-l)) || ((i-k) == (l-j))) && i!=k && j!=l) {
+            arr[k][l] == 0;
+          }
+        }
+      }
+    }
+  }
+
+
+  function void print();
+    for (int j = 0; j < N; j++) begin
+      for (int i = 0; i < N; i++) begin
+        $write(" %c", arr[i][j] ? "Q" : ".");
+      end
+      $write("|\n");
+    end
+  endfunction: print
+
+endclass : queens
+
+
+module main ();
+  initial begin
+    queens h;
+    h = new();
+    repeat (1) begin
+      assert(randomize(h));
+      $display("===============================================");
+      h.print();
+    end
+  end
+endmodule
