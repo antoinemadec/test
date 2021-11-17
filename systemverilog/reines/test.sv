2cd7e75955ef9613fa8cc09ba17088daa28ad015
EXTERNAL MADEC Antoine (ELSYS, AE/EIY2) <external.Antoine.MADEC@fr.bosch.com>
2021-11-17 10:15:23 +0100

    SV: queens, faster N queens conditions

--- systemverilog/reines/test.sv
+++ systemverilog/reines/test.sv
@@ -1,25 +1,27 @@
 class queens;
-  localparam N = 8;
+  localparam N = 70;
 
   rand bit arr[N][N];
 
   constraint c {
-    // N queens
-    arr.sum(item1d) with (item1d.sum(item2d) with (int'(item2d))) == N;
+    // N queens, one per column
+    foreach (arr[i]) {
+      arr[i].sum(item) with (int'(item))  == 1;
+    }
 
     foreach (arr[i, j]) {
       if (arr[i][j] == 1) {
-        // only one 1 in a col
-        foreach (arr[ , l]) {
-          if (j != l) {
-            arr[i][l] == 0;
-          }
-        }
-
         // only one 1 in a row
         foreach (arr[k, ]) {
           if (i != k) {
             arr[k][j] == 0;
+          }
+        }
+
+        // only one 1 in a col
+        foreach (arr[ , l]) {
+          if (j != l) {
+            arr[i][l] == 0;
           }
         }
 
@@ -52,8 +54,8 @@ module main ();
     h = new();
     repeat (1) begin
       assert(randomize(h));
-      $display("===============================================");
-      h.print();
     end
+    $display("===============================================");
+    h.print();
   end
 endmodule
