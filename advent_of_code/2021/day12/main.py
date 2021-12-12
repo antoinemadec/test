11e153db19340a3a871577b582580984191127bc
antoinemadec <ajamadec@gmail.com>
2021-12-12 16:05:38 +0100

    advent_of_code: day12 use cache for better performances

--- advent_of_code/2021/day12/main.py
+++ advent_of_code/2021/day12/main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import time
+
+
 class Cave:
     def __init__(self, name: str):
         self.name = name
@@ -19,6 +22,7 @@ class Paths:
         self.caves: dict[str, Cave] = {}
         self.populate_caves(lines)
         self.p1 = p1
+        self.find_paths_cache = {}
 
     def populate_caves(self, lines):
         for l in lines:
@@ -34,31 +38,29 @@ class Paths:
             if 2 in counts:
                 return False
         else:
-            if counts and max(counts) > 2:
+            if counts and max(counts) > 2 or len([1 for i in counts if i == 2]) > 1:
                 return False
-            else:
-                if 2 in counts:
-                    counts.pop(counts.index(2))
-                if 2 in counts:
-                    return False
         return True
 
     def find_paths(self, name="start", small_cave_visit_count: dict = {}):
-        # update visit count
-        if self.caves[name].is_small:
-            small_cave_visit_count[name] = small_cave_visit_count.get(
-                name, 0) + 1
+        key = f"{name} {small_cave_visit_count}"
+        if key not in self.find_paths_cache:
+            cave = self.caves[name]
 
-        # call find_paths() recursively if visit_count condition is OK
-        if self.small_cave_condition_is_ok(small_cave_visit_count):
-            paths = [[name]] if name == "end" else []
-            for connection in self.caves[name].connections:
-                for path in self.find_paths(connection, small_cave_visit_count.copy()):
-                    new_path = [name] + path
-                    paths.append(new_path)
-            return paths
-        else:
-            return []
+            # update visit count
+            if cave.is_small:
+                small_cave_visit_count = small_cave_visit_count.copy()
+                small_cave_visit_count[name] = small_cave_visit_count.get(name, 0) + 1
+
+            # call find_paths() recursively if visit_count condition is OK
+            paths = [["end"]] if name == "end" else []
+            if self.small_cave_condition_is_ok(small_cave_visit_count):
+                for connection in cave.connections:
+                    for path in self.find_paths(connection, small_cave_visit_count):
+                        paths.append([name] + path)
+            self.find_paths_cache[key] = paths
+
+        return self.find_paths_cache[key]
 
 
 def p1(lines):
@@ -73,5 +75,10 @@ def p2(lines):
 
 with open('input.txt', 'r') as f:
     lines = [l.strip() for l in f.readlines()]
+    t0 = time.time()
     print(p1(lines))
+    t1 = time.time()
     print(p2(lines))
+    t2 = time.time()
+    print(f"part1 {t1-t0:.2f} s")
+    print(f"part2 {t2-t1:.2f} s")
