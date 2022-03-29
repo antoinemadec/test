75194ed52899a2e73f02018e94974fbfcc8bdaa9
EXTERNAL MADEC Antoine (ELSYS, AE/EIY2) <external.Antoine.MADEC@fr.bosch.com>
2022-03-29 17:41:39 +0200

    UVM: add coverage in fifo example

--- uvm/easier_uvm/fifo/generated_tb/tb/top/sv/top_config.sv
+++ uvm/easier_uvm/fifo/generated_tb/tb/top/sv/top_config.sv
@@ -21,12 +21,12 @@ function top_config::new(string name = "");
   m_fifo_in_config                  = new("m_fifo_in_config"); 
   m_fifo_in_config.is_active        = UVM_ACTIVE;              
   m_fifo_in_config.checks_enable    = 1;                       
-  m_fifo_in_config.coverage_enable  = 0;                       
+  m_fifo_in_config.coverage_enable  = $test$plusargs("coverage_enable") ? 1:0;
 
   m_fifo_out_config                 = new("m_fifo_out_config");
   m_fifo_out_config.is_active       = UVM_ACTIVE;              
   m_fifo_out_config.checks_enable   = 1;                       
-  m_fifo_out_config.coverage_enable = 0;                       
+  m_fifo_out_config.coverage_enable  = $test$plusargs("coverage_enable") ? 1:0;
 
 endfunction : new
 
